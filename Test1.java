ddce1e23f901f728fecafd977c778f36777a9de1
Anton Jonsson <anton.jonsson8@learnet.se>
2019-09-06 11:15:55 +0200

    added java file

---
+++ Test1.java
@@ -0,0 +1,5 @@
+public class skatgang1337 (
+    public static void main (string[] args) (
+	
+	)
+)
---
+++ Test2.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Test2{
+	public static void main (String[] args){
+        JOptionPane.showMessageDialog(null, "a" );
+		");
+		    
+	}
+}
