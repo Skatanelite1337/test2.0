8afeb4426009f879b224f35f25dfee783bb2f99f
Anton Jonsson <anton.jonsson8@learnet.se>
2019-09-13 11:19:39 +0200

    la till kod

--- Test2.java
+++ Test2.java
@@ -1,9 +1,26 @@
+/*
+Ett enkelt Hello World i java
+Skapat 2019-09-13
+Programmerare Anton Jonsson
+*/
 import javax.swing.*;
+import java.util.Scanner;
 
-public class Test2{
-	public static void main (String[] args){
-        JOptionPane.showMessageDialog(null, "a" );
-		");
-		    
+public class Test2 {
+	public static void main(String[] args) {
+		// variabledeklaration
+		String msg;
+		// tilldelning
+		msg = "nu har variabeln ett värde";
+		// initiering
+		int tal = 17;
+		//dialogruta
+		msg = JOptionPane.showInputDialog(null, "Skriv ett medelande");
+		JOptionPane.showMessageDialog(null, msg);
+		// I terminalen
+		System.out.println("Skriv ett medelande");
+		Scanner tgb = new Scanner(System.in);
+		msg = tgb.nextLine();
+		System.out.println(msg);
 	}
 }
